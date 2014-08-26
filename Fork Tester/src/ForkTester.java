af3f079b7b8d48c907530bb9f83a3c9d45f16b6b
mrmikemcguire <mcguire@mullenhigh.com>
2014-08-26 08:11:10 -0600

    Initial commit

---
+++ Fork Tester/src/ForkTester.java
@@ -0,0 +1,16 @@
+public class ForkTester
+	{
+
+	public static void main(String[] args)
+		{
+		// Try what you did before.  
+		// Fork this to your personal GitHub account.
+		// Download it to school and add a line.
+		// Commit that back to your GitHub account.
+		// Download it to your home local repo.
+		// Commit that back to GitHub.
+		// Update your local school repo and show me.
+
+		}
+
+	}
